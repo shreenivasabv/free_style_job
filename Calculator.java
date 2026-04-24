359383a0eaa4508829c3e8d20b5078a06015d108
Shreenivasa Murthy B V <179219653+shreenivasabv@users.noreply.github.com>
2026-04-24 10:15:52 +0530

    Update and rename calculator.java to Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class calculator{
+public class Calculator{
 	public static void main(String[] args){
 	Scanner sc = new Scanner(System.in);
 	System.out.println("enter a");
